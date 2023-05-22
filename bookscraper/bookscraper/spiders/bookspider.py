ceabaa2ad7e29eced9c71413ab38d6980d04f4a4
Haruto Mori <baystarsman@outlook.jp>
2023-05-23 00:19:25 +0900

    go to the book page

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -10,17 +10,24 @@ class BookspiderSpider(scrapy.Spider):
         books = response.css('article.product_pod')
 
         for book in books:
-            yield {
-                'name': book.css('h3 a::text').get(),
-                'price': book.css('.product_price .price_color::text').get(),
-                'url': book.css('h3 a').attrib['href'],
-            }
-        
-        next_page = response.css('li.next a ::attr(href)').get()
+            relative_url = book.css('h3 a ::attr(href)').get()
 
-        if next_page is not None:
-            if 'catalogue/' in next_page:
-                next_page_url = 'https://books.toscrape.com/' + next_page
+            if 'catalogue/' in relative_url:
+                book_url = 'https://books.toscrape.com/' + relative_url
             else:
-                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
-            yield response.follow(next_page_url, callback=self.parse)
+                book_url = 'https://books.toscrape.com/catalogue/' + relative_url
+            yield response.follow(book_url, callback=self.parse_book_page)
+
+        
+        # next_page = response.css('li.next a ::attr(href)').get()
+
+        # if next_page is not None:
+        #     if 'catalogue/' in next_page:
+        #         next_page_url = 'https://books.toscrape.com/' + next_page
+        #     else:
+        #         next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+        #     yield response.follow(next_page_url, callback=self.parse)
+
+
+    def parse_book_page(self, response):
+        pass
